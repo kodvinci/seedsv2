3379698c742de1e78cfe24506f1478c68f88620c
Leonard Ngeno <leonardngeno@Rachels-MacBook-Pro.local>
2012-06-13 19:07:49 +0300

    lender

--- app/models/lender.rb
+++ app/models/lender.rb
@@ -1,6 +1,6 @@
 class Lender < ActiveRecord::Base
     belongs_to :user
-    attr_accessible :first_name, :last_name, :credit, :user_id, :UID, :exp, :luid
+    attr_accessible :first_name, :last_name, :credit, :user_id, :UID, :exp, :luid, :class_type
     validates_presence_of :first_name, :last_name
 
 end
