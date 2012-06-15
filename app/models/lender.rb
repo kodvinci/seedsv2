94b9c0433bd689a612746afc8ecb136026d889fa
Leonard Ngeno <leonardngeno@Rachels-MacBook-Pro.local>
2012-06-15 11:47:09 +0300

    heroku

--- app/models/lender.rb
+++ app/models/lender.rb
@@ -1,6 +1,6 @@
 class Lender < ActiveRecord::Base
     belongs_to :user
-    attr_accessible :first_name, :last_name, :credit, :user_id, :UID, :exp, :luid, :class_type
+    attr_accessible :first_name, :last_name, :credit, :user_id, :exp, :luid, :class_type
     validates_presence_of :first_name, :last_name
 
 end
