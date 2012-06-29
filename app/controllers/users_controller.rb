42c0e042b77d960b49e4a680a1363fec6c4e54e5
Leonard Ngeno <leonardngeno@Rachels-MacBook-Pro.local>
2012-06-29 12:05:51 +0300

    before_filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-    before_filter :authenticate_user!
+    before_filter :authenticate_user!, :only => [:index, :show]
     
   def index
     @users = User.all
