46994d3048d55db8d8992d20ea7d98fab7b2bfe5
Leonard <dranoel.bravo@gmail.com>
2012-09-23 16:11:49 -0400

    user controller methods: edit & update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,11 @@ class UsersController < ApplicationController
     @user = User.new
     @lender = Lender.new
   end
-
+    
+def edit
+    @user = User.find(params[:id])
+end
+    
   def create
     @user = User.new(params[:user])
     @lender = @user.lender.build(params[:lender])
@@ -26,6 +30,20 @@ class UsersController < ApplicationController
       render :action => 'new'
     end
   end
+ 
+    def update
+        @user = User.find(params[:id])
+        
+        respond_to do |format|
+            if @user.update_attributes(params[:user])
+                format.html { redirect_to @user, notice: 'User was successfully updated.' }
+                format.json { head :ok }
+                else
+                format.html { render action: "edit" }
+                format.json { render json: @user.errors, status: :unprocessable_entity }
+            end
+        end
+    end
     
   def byEmail
       @user = User.find(params[:email])
