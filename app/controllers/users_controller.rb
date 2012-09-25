ba5ca10816e2a6d9b380e7f6dd542c6a5e0d78df
Leonard <dranoel.bravo@gmail.com>
2012-09-25 15:09:52 -0400

    format json html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,31 +4,55 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end    
   end
 
   def show
     @user = User.find(params[:id])
+    
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
   end
 
   def new
     @user = User.new
-    @lender = Lender.new
+    
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @user }
+    end
   end
     
 def edit
     @user = User.find(params[:id])
 end
     
-  def create
-    @user = User.new(params[:user])
-    @lender = @user.lender.build(params[:lender])
-    if @user.save
-      flash[:notice] = "Successfully signed up as a Seeds lender!"
-      redirect_to :action => 'show'
-    else
-      render :action => 'new'
+def create
+  @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
-  end
+
+#    if @user.save
+#      flash[:notice] = "Successfully signed up as a Seeds lender!"
+#      redirect_to :action => 'show'
+#    else
+#      render :action => 'new'
+#    end
+end
  
     def update
         @user = User.find(params[:id])
